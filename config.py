067d4678b5777070a70b842ff49bb3458742f14c
Sofia Sovkova <sofiasovkova@gmail.com>
2024-12-07 12:28:38 +0300

    нельзя так делать...

--- config.py
+++ config.py
@@ -1,14 +1,9 @@
-# gpt request
-HTTPS_PROXY_IPPORT = "168.228.47.39:9023"
-HTTPS_PROXY_LOGIN = "9ZkHS4"
-HTTPS_PROXY_PASSWORD = "4ebFZj"
-OPENAI_API_KEY = "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"
+HTTPS_PROXY_IPPORT = ""
+HTTPS_PROXY_LOGIN = ""
+HTTPS_PROXY_PASSWORD = ""
+OPENAI_API_KEY = ""
 
 # бот
-TOKEN = '7762120638:AAEn-KaM6kWue3UZVqNMtHz8VrChQGeRIO0'
+TOKEN = ''
 
-# путь до базы данных
-BD_path = "vkusvill_products.json"
-
-# время обновление бд [часы, минусы]
-UPDATE_BD_TIME = (20, 00)
+BD_path = ""
